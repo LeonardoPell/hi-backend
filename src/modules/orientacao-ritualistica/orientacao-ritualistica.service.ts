07f2a4e1608ca612cab78df3e4b7f637670c869e
LeonardoPell <leonardojbpellegrino@gmail.com>
2023-06-24 23:11:51 -0300

    feat: orientacao ritualistica service

--- src/modules/orientacao-ritualistica/orientacao-ritualistica.service.ts
+++ src/modules/orientacao-ritualistica/orientacao-ritualistica.service.ts
@@ -3,6 +3,12 @@ import { CreateOrientacaoRitualisticaDto } from './dto/create-orientacao-rituali
 import { UpdateOrientacaoRitualisticaDto } from './dto/update-orientacao-ritualistica.dto';
 import { OrientacaoRitualistica } from './entities/orientacao-ritualistica.entity';
 import { InjectModel } from '@nestjs/sequelize';
+import { RetornoApi } from 'src/global/interface/retornoApi.model';
+import { createdReturn } from 'src/global/functions/created.model';
+import { errorTryCatchReturn } from 'src/global/functions/error-try-catch.model';
+import { notFoundReturn } from 'src/global/functions/not-found.model';
+import { foundReturn } from 'src/global/functions/found.model';
+import { updatedReturn } from 'src/global/functions/updated.model';
 
 @Injectable()
 export class OrientacaoRitualisticaService {
@@ -12,23 +18,66 @@ export class OrientacaoRitualisticaService {
     private readonly orientacaoRitualistica: typeof OrientacaoRitualistica,
   ){}
 
-  create(createOrientacaoRitualisticaDto: CreateOrientacaoRitualisticaDto) {
-    return 'This action adds a new orientacaoRitualistica';
+  async create(createOrientacaoRitualisticaDto: CreateOrientacaoRitualisticaDto): Promise<RetornoApi> {
+    try {
+      const orientacaoRitualistica = await this.orientacaoRitualistica.create(createOrientacaoRitualisticaDto as any);
+
+      return createdReturn('Documento cadastrado com sucesso!', orientacaoRitualistica);
+    } catch (error) {
+      return errorTryCatchReturn(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all orientacaoRitualistica`;
+  async findAll(): Promise<RetornoApi> {
+    try {
+      const orientacoesRitualisticas = await this.orientacaoRitualistica.findAll();
+
+      if (!orientacoesRitualisticas.length) {
+        return notFoundReturn(
+          `Nenhum documento foi encontrado no sistema!`,
+        );
+      }
+
+      return foundReturn('Documentos encontrados com suecesso!', orientacoesRitualisticas);
+    } catch (error) {
+      return errorTryCatchReturn(error);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} orientacaoRitualistica`;
+  async findOne(id: number): Promise<RetornoApi>  {
+    try {
+      const orientacaoRitualistica = await this.orientacaoRitualistica.findByPk(id);
+
+      if (!orientacaoRitualistica) {
+        return notFoundReturn(
+          `Nenhum documento com id ${id} foi encontrado no sistema!`,
+        );
+      }
+
+      return foundReturn('Documento encontrado com suecesso!', orientacaoRitualistica);
+    } catch (error) {
+      return errorTryCatchReturn(error);
+    }
   }
 
-  update(id: number, updateOrientacaoRitualisticaDto: UpdateOrientacaoRitualisticaDto) {
-    return `This action updates a #${id} orientacaoRitualistica`;
-  }
+  async update(id: number, updateOrientacaoRitualisticaDto: UpdateOrientacaoRitualisticaDto): Promise<RetornoApi>  {
+    try {
+      const orientacaoRitualistica = await this.orientacaoRitualistica.findByPk(id);
 
-  remove(id: number) {
-    return `This action removes a #${id} orientacaoRitualistica`;
+      if(!orientacaoRitualistica){
+        return notFoundReturn(
+          `Nenhum documento com id ${id} foi encontrado no sistema!`,
+        );
+      }
+      orientacaoRitualistica.set(updateOrientacaoRitualisticaDto as any);
+      await orientacaoRitualistica.save();
+
+      return updatedReturn(
+        `Documento de id ${id} editado com sucesso!`,
+        orientacaoRitualistica,
+      );
+    } catch (error) {
+      return errorTryCatchReturn(error);
+    }
   }
 }
